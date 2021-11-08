b5912a7fa549106d0955fbed7925a903183e8bd6
BulatXam <Khamdbulat@yandex.ru>
2021-11-08 20:34:29 +0300

    feat: add engine for sqlalchemy

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-TOKEN="2120001268:AAEl-aoSg7TiZjHvYZ6XrtS-sl5FxFN_Z2Y"
+from sqlalchemy import create_engine
+
+TOKEN = "2120001268:AAEl-aoSg7TiZjHvYZ6XrtS-sl5FxFN_Z2Y"
+DB_ENGINE = create_engine('sqlite:///db.sqlite3', echo=False)
