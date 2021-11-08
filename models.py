db2c4cdbdfcd836ed07a835fe9ba6d5d4dbd1e84
BulatXam <Khamdbulat@yandex.ru>
2021-11-08 20:32:46 +0300

    feat: add author and channel tables for db

--- models.py
+++ models.py
@@ -1,20 +1,41 @@
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///db.sqlite3', echo=False)
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
+class Author(Base):
+    __tablename__ = "author"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String, nullable=True)
+    username = Column(String, nullable=True)
+
+    channels = relationship("Channel", backref="channels")
+
+
 class Channel(Base):
     __tablename__ = 'channel'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    link = Column(String)
+    id = Column(String, primary_key=True)
+    author_id = Column(Integer, ForeignKey("author.id", ondelete="CASCADE"))
 
-    def __init__(self, name, link):
-        self.name = name
-        self.link = link
+    title = Column(String)
+    username = Column(String)
+    description = Column(String)
+    invite_link = Column(String)
+    linked_chat_id = Column(String)
+
+    def __init__(self, id, author_id, title, username, description,
+                 invite_link, linked_chat_id):
+        self.id = id
+        self.author_id = author_id
+        self.title = title
+        self.username = username
+        self.description = description
+        self.invite_link = invite_link
+        self.linked_chat_id = linked_chat_id
 
     def __repr__(self):
         return f'Channel<name="{self.name}",' \
@@ -22,4 +43,7 @@ class Channel(Base):
 
 
 if __name__ == "__main__":
+    from sqlalchemy import create_engine
+
+    engine = create_engine('sqlite:///db.sqlite3', echo=False)
     Base.metadata.create_all(engine)
